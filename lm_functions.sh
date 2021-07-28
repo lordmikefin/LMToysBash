c6114b250a407bb89e96a4a32c55b8a18d695897
Lord Mike <lordmike@iki.fi>
2021-07-28 16:30:20 +0300

    Tesing os with 'case' instead of 'if'

--- lm_functions.sh
+++ lm_functions.sh
@@ -83,7 +83,7 @@
 
 unset LM_FUNCTIONS_VER LM_FUNCTIONS_DATE LM_FUNCTIONS_LOADED
 LM_FUNCTIONS_LOADED=false
-LM_FUNCTIONS_VER="1.3.5"
+LM_FUNCTIONS_VER="1.3.6"
 LM_FUNCTIONS_DATE="2021-07-28"
 #echo "LM functions version: ${LM_FUNCTIONS_VER} (${LM_FUNCTIONS_DATE})"
 
@@ -112,11 +112,21 @@ unset OS_NAME OS_VER OS_ARCH CURRENT_SHELL INCORRECT_VERSION
 OS_NAME="$(uname)"
 OS_VER="$(uname -r)"
 OS_ARCH="$(uname -m)"
-if [[ ${OS_NAME} != "Linux" ]] ; then
-	>&2 echo -e "\n Current OS name: ${OS_NAME}"
-	>&2 echo -e " System is not Linux. This script is tested only with Linux.  Aborting."
-	exit 1
-fi
+case "${OS_NAME}" in
+	"Linux" )
+		# Linux is OK
+		;;
+	* )
+		>&2 echo -e ""
+		>&2 echo -e "Current OS name: ${OS_NAME}"
+		>&2 echo -e "System is not supported.  Aborting."
+		exit 1
+esac
+
+
+echo "This script is in test mode :)  Aborting." >&2
+exit 1 # 127
+
 
 CURRENT_SHELL="$(basename $SHELL)"
 if [[ ${CURRENT_SHELL} != "bash" ]] ; then
